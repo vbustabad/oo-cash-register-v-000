9f782c4001ebff85ac20763087c5997545b88967
Veronica Bustabad <vbustabad@gmail.com>
2018-03-29 20:37:22 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ class CashRegister
   end
 
   def items
-    items = items + cash_register.add_item(title, price, quantity = 1)
+    items = items + self.add_item(title, price, quantity = 1)
   end
 
   def void_last_transaction
